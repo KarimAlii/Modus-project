cdd1f82f614e23bb036c8305ffc66818f514c0bf
KarimMocka <kariim.alii1@gmail.com>
2021-12-28 01:59:01 +0200

    uploading

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
-#!/usr/bin/env sh
-
-# abort on errors
-set -e
 
 # build
 npm run build
 
+LOGSTRING=$(git log)
+COMMIT=$(echo $LOGSTRING | awk'{print $2}')
 # navigate into the build output directory
 cd dist
 
@@ -14,12 +12,12 @@ cd dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "deploy (commit: $COMMIT)"
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:KarimMocka/modus-project.git main:gh-pages
+git push -f https://github.com/KarimMocka/modus-project.git :gh-pages
 
 cd -
